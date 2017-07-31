42fef05a38220e497e5fdcce62c3c864618c78db
viet <viettangle12@gmail.com>
2017-07-30 21:16:51 -0400

    starting google oauth

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //no es2015 for nodejs, but nodejs is using commonjs module
 const express = require("express");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 //route handler
 app.get("/", (req, res) => {
-  res.send({ hello: "buddy" });
+  res.send({ hello: "Viet" });
 });
 
 //dynamically which port listen to
