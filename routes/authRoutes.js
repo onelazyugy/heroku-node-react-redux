bdd972cb66d6232fc5286cf56a0fd513d6e8efa2
viet <viettangle12@gmail.com>
2017-08-14 22:10:20 -0400

    add logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,13 @@ module.exports = (app) => {
 
   //send the code back to google in exchange for email and user profile
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout(); //logout() is attached to req by passport
+    res.send(req.user);
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  })
 };
