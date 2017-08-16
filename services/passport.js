e19b08206245679baec40899f16e69b470d2a5fc
viet <viettangle12@gmail.com>
2017-08-15 22:45:33 -0400

    finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     //callback function
     (accessToken, refreshToken, profile, done) => {
