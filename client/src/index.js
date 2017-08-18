008d2260bfd41222e62c06a4d26845f10a1a0732
viet <viettangle12@gmail.com>
2017-08-17 23:27:29 -0400

    wire up react index and app js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//this is start of the app, render root app component to dom
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
