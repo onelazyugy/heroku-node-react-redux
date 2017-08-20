edaf45b3d17d8995b34cc1942700350b34c7672f
viet <viettangle12@gmail.com>
2017-08-20 15:24:18 -0400

    add header using material css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Header
-            </div>
-        );
-    }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
