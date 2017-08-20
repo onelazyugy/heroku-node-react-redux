9f2bdc9b517572b4a7ab1b59c8dd5ebd39c1777d
viet <viettangle12@gmail.com>
2017-08-20 14:40:58 -0400

    wire up redux store and react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,22 @@
 //this is start of the app, render root app component to dom
 import React from "react";
 import ReactDOM from "react-dom";
+//Provider is a react component that glues redux with react
+//it allows components to reach into the redux store for data anywhere in the component hierarchy 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+//root reducers
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+//redux store
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+
+//root reducer
+export default combineReducers({
+    auth: authReducer
+});
