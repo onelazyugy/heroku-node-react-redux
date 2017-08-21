4415a66d72888b6f18587c49ea4b9499ac4379ba
viet <viettangle12@gmail.com>
2017-08-20 22:23:56 -0400

    wrap up header and link from logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -8,11 +9,17 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return(
-            <li><a href="/auth/google">Login With Google</a></li>
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
     }
   }
 
@@ -21,7 +28,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
